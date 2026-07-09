3e62009003070e49cab2be3f5e7d38d94a6281be
Vlad Finiak <vlad.finyak4201@gmail.com>
2026-07-09 16:26:14 +0200

    XGBoost model

---
+++ GridSearchCV_xgb.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix, classification_report
+import os
+
+param_grid = {
+    'n_estimators': [50, 100, 150, 200],
+    'max_depth': [2, 3, 4],
+    'learning_rate': [0.05, 0.1, 0.2],
+    'scale_pos_weight': [3,4,5]
+}
+
+
+df = pd.read_csv('archive\\data_ecommerce_customer_churn.csv')
+df = pd.get_dummies(df, columns=["PreferedOrderCat", "MaritalStatus"])
+
+y = df['Churn']
+X = df.drop('Churn', axis=1)
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2)
+
+base_xgb = XGBClassifier(random_state=42)
+
+grid_search = GridSearchCV(
+    estimator=base_xgb, 
+    param_grid=param_grid, 
+    cv=5, 
+    # scoring='accuracy', #or recall, precision
+    verbose=1
+)
+
+print("Starting grid search...")
+grid_search.fit(X_train, y_train)
+
+print(f"The Best Parameters are: {grid_search.best_params_}")
